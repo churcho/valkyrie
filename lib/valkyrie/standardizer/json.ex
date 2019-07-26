937723c8339a900b0ea579d5b6c0603fc0f72e0e
JarredOlson <jarred.olson@accenture.com>
2019-07-26 18:03:12 -0400

    Reverting back to old structure
    co-authored-by: Austin Kucera <akucera@pillartechnology.com>

--- lib/valkyrie/standardizer/json.ex
+++
@@ -1,9 +0,0 @@
-defmodule Valkyrie.Standardizer.Json do
-  @moduledoc false
-  def standardize(_, value) do
-    case Jason.encode(value) do
-      {:ok, result} -> {:ok, result}
-      _ -> {:error, :invalid_json}
-    end
-  end
-end
--- lib/valkyrie/standardizer/string.ex
+++
@@ -1,8 +0,0 @@
-defmodule Valkyrie.Standardizer.String do
-  @moduledoc false
-  def standardize(_, value) do
-    {:ok, to_string(value)}
-  rescue
-    Protocol.UndefinedError -> {:error, :invalid_string}
-  end
-end
